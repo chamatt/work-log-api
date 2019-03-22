93bbb75bf3753894ccff4ba64ca7f42ec06d4918
chamatt <matt.vicent@gmail.com>
2019-03-22 15:23:31 -0300

    Funciona pls

--- server.js
+++ server.js
@@ -12,14 +12,14 @@ const categories = require("./routes/api/categories");
 
 app.use(cors());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan("combined"));
